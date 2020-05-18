df9236a3c33b487b29b506240bbed2fc7860c4af
Ankur Atri <ankuratri@hotmail.com>
2020-05-18 14:08:54 +0530

    route and query params

--- 4-building-RESTful-APIs-using-express/express-demo/index.js
+++ 4-building-RESTful-APIs-using-express/express-demo/index.js
@@ -9,4 +9,22 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => "Listening on port 3000...");
+// route parameters
+// apiUrl: "/api/courses/1"
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+// apiUrl: "/api/posts/2018/1"
+// app.get("/api/posts/:year/:month", (req, res) => {
+//   res.send(req.params);
+// });
+
+// query params
+// apiUrl: "/api/posts/2018/1?sortBy=name"
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
