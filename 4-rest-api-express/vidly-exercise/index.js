65792710d07707399ffe25297a5e648a8a7272db
Ankur Atri <ankuratri@hotmail.com>
2020-05-24 22:52:32 +0530

    vidly app code refactored

--- 4-rest-api-express/vidly-exercise/index.js
+++ 4-rest-api-express/vidly-exercise/index.js
@@ -1,81 +1,17 @@
 const Joi = require("@hapi/joi");
 const express = require("express");
 const app = express();
+const genres = require("./routes/genres");
+const home = require("./routes/home");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Comedy" },
-  { id: 3, name: "Thriller" },
-];
+// route for genres
+app.use("/api/genres", genres);
 
-// app home
-app.get("/", (req, res) => {
-  res.send("Welcome to Vidly App!");
-});
-
-// get genres
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
-
-// get single genre using id as route param
-app.get("/api/genres/:id", (req, res) => {
-  const genre = findGenre(req.params.id);
-  if (!genre) return res.status(404).send("Genre with given ID was not found.");
-
-  res.send(genre);
-});
-
-// adding new genre
-app.post("/api/genres", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(genre);
-
-  res.send(genre);
-});
-
-// updating genre using id as route param
-app.put("/api/genres/:id", (req, res) => {
-  const genre = findGenre(req.params.id);
-  if (!genre) return res.status(404).send("Genre with given ID was not found.");
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-// delete genre using id as route param
-app.delete("/api/genres/:id", (req, res) => {
-  const genre = findGenre(req.params.id);
-  if (!genre) return res.status(404).send("Genre with given ID was not found.");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genres);
-});
-
-// common functions
-function findGenre(id) {
-  return genres.find((g) => g.id === parseInt(id));
-}
-
-function validateGenre(genre) {
-  const schema = Joi.object({
-    name: Joi.string().required(),
-  });
-  return schema.validate(genre);
-}
+// route for home
+app.use("/", home);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
