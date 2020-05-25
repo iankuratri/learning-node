3f30aea65f394c026fa92f2081ebb189e660bb62
Ankur Atri <ankuratri@hotmail.com>
2020-05-25 13:55:28 +0530

    fuction names simplified

--- 5-async-js/async-demo/callback.js
+++ 5-async-js/async-demo/callback.js
@@ -1,13 +1,13 @@
 console.log("Before");
 
-fetchUser(1, getRepositories);
+getUser(1, fetchRepositories);
 
-function getRepositories(user) {
-  fetchRepositories(user.gitHubUsername, getCommits);
+function fetchRepositories(user) {
+  getRepositories(user.gitHubUsername, fetchCommits);
 }
 
-function getCommits(repos) {
-  fetchCommits(repos[0], displayCommits);
+function fetchCommits(repos) {
+  getCommits(repos[0], displayCommits);
 }
 
 function displayCommits(commits) {
@@ -16,7 +16,7 @@ function displayCommits(commits) {
 
 console.log("After");
 
-function fetchUser(id, callback) {
+function getUser(id, callback) {
   // async code
   setTimeout(() => {
     console.log("Reading a user from database...");
@@ -24,14 +24,14 @@ function fetchUser(id, callback) {
   }, 2000);
 }
 
-function fetchRepositories(username, callback) {
+function getRepositories(username, callback) {
   setTimeout(() => {
     console.log("Getting user repo's from database...");
     callback(["repo 1", "repo 2", "repo 3"]);
   }, 2000);
 }
 
-function fetchCommits(repo, callback) {
+function getCommits(repo, callback) {
   setTimeout(() => {
     console.log("Getting repo commits from database...");
     callback(["commit 1", "commit 2", "commit 3"]);
