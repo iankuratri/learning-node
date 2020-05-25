c0bf58169af4b6f4f81afb5e979f95dae6cab821
Ankur Atri <ankuratri@hotmail.com>
2020-05-25 13:52:49 +0530

    callbacks flattened to get rid of callback hell

--- 5-async-js/async-demo/callback.js
+++ 5-async-js/async-demo/callback.js
@@ -1,35 +1,39 @@
 console.log("Before");
 
-getUser(1, (user) => {
-  console.log("User:", user);
-  getRepositories(user.gitHubUsername, (repos) => {
-    console.log("Repos:", repos);
-    getCommits(repos[1], (commits) => {
-      console.log("Comiits", commits);
-    });
-  });
-});
+fetchUser(1, getRepositories);
+
+function getRepositories(user) {
+  fetchRepositories(user.gitHubUsername, getCommits);
+}
+
+function getCommits(repos) {
+  fetchCommits(repos[0], displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log("Commits:", commits);
+}
 
 console.log("After");
 
-function getUser(id, callback) {
+function fetchUser(id, callback) {
   // async code
   setTimeout(() => {
     console.log("Reading a user from database...");
-    callback({ id: id, gitHubUsername: "iankuratri" });
+    callback({ id, gitHubUsername: "iankuratri" });
   }, 2000);
 }
 
-function getRepositories(username, callback) {
+function fetchRepositories(username, callback) {
   setTimeout(() => {
     console.log("Getting user repo's from database...");
     callback(["repo 1", "repo 2", "repo 3"]);
   }, 2000);
 }
 
-function getCommits(repo, callback) {
+function fetchCommits(repo, callback) {
   setTimeout(() => {
     console.log("Getting repo commits from database...");
-    callback({ repo, commits: ["commit 1", "commit 2", "commit 3"] });
+    callback(["commit 1", "commit 2", "commit 3"]);
   }, 2000);
 }
