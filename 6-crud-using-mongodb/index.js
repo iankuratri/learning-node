8f6f57e08cada701f8fa68512408ce80eb69dafd
Ankur Atri <ankuratri@hotmail.com>
2020-05-26 15:51:16 +0530

    minor changes

--- 6-crud-using-mongodb/index.js
+++ 6-crud-using-mongodb/index.js
@@ -83,8 +83,6 @@ async function getCourse() {
   // .find({ author: /.*Mosh.*/i })
 
   const courses = await Course.find({ author: "Mosh", isPublished: true })
-
-    .find({ author: /^Mosh/ })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
