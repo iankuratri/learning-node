e1797d97896b6563c94ca37610f9160ee39e6c2b
Ankur Atri <ankuratri@hotmail.com>
2020-06-04 14:21:23 +0530

    mongoose added in vidly app

--- vidly-app/routes/genres.js
+++ vidly-app/routes/genres.js
@@ -1,6 +1,23 @@
 const Joi = require("@hapi/joi");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/vidly-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+// Schema
+const genreSchema = new mongoose.Schema({
+  name: { type: string, required: true },
+});
+
+// Model
+const Genre = new mongoose.model("Genre", genreSchema);
 
 const genres = [
   { id: 1, name: "Action" },
@@ -8,8 +25,20 @@ const genres = [
   { id: 3, name: "Thriller" },
 ];
 
+// add a genre to db
+async function createGenre() {
+  const genre = new Genre({ name: "Sci-Fi" });
+  try {
+    const result = await genre.save();
+    console.log(result);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+createGenre();
+
 // get genres
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.send(genres);
 });
 
