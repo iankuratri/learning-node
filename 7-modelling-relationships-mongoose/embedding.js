8bf8853c1e74fd7ac488bd86b9c2e8c142d950aa
Ankur Atri <ankuratri@hotmail.com>
2020-06-14 13:30:17 +0530

    embedding documents example added

--- 7-modelling-relationships-mongoose/embedding.js
+++ 7-modelling-relationships-mongoose/embedding.js
@@ -1,34 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const authorSchema = new mongoose.Schema({
   name: String,
   bio: String,
-  website: String
+  website: String,
 });
 
-const Author = mongoose.model('Author', authorSchema);
+const Author = mongoose.model("Author", authorSchema);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String
-}));
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: { type: authorSchema, required: true },
+  })
+);
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
+    name,
+    author,
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course.find();
   console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function updateAuthor(courseId) {
+  /**
+   * const course = await Course.findById(courseId);
+   * course.author.name = "Mosh Hamedani";
+   * course.save();
+   */
+
+  // update directly in db
+  const course = await Course.update(
+    { _id: courseId },
+    {
+      $set: {
+        "author.name": "Stephen Grider",
+      },
+    }
+  );
+
+  // $unset: { "author": "" }
+}
+
+// createCourse("Node Course", new Author({ name: "Mosh" }));
+
+// updateAuthor("5ee5d66c9e882a373432913f");
