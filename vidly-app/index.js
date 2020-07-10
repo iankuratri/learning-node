a4b5e632ed43094b0f429c88c621f416b2fef166
Ankur Atri <ankuratri.in@gmail.com>
2020-07-10 15:03:07 +0530

    error handling done

--- vidly-app/index.js
+++ vidly-app/index.js
@@ -9,10 +9,33 @@ const config = require("config");
 const express = require("express");
 const app = express();
 
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
+// handle uncaught excepton
+// process.on("uncaughtException", (ex) => {
+//   console.log("WE GOT AN UNCAUGHT EXCEPTION");
+//   winston.error(ex.message, ex);
+// });
+
+// handle unhandledRejection
+process.on("unhandledRejection", (ex) => {
+  throw ex;
+});
+
+winston.add(
+  new winston.transports.File({
+    filename: "logfile.log",
+    handleExceptions: true,
+  })
+);
 winston.add(
   new winston.transports.MongoDB({ db: "mongodb://localhost:27017/vidly-app" })
 );
+
+// for simulating uncaughtException error
+// throw new Error("Something failed during startup.")
+
+// for simulating unhandledRejection error
+// const p = Promise.reject(new Error("Something failed misrerably"));
+// p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
