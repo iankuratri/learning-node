87d411e324b8fc7fb4fd7b2f656eea2e805e0c8a
Ankur Atri <ankuratri.in@gmail.com>
2020-07-13 15:05:53 +0530

    tests added for strings, arrays and objects

---
+++ 10-unit-testing/tests/lib.test.js
@@ -0,0 +1,39 @@
+const lib = require("../lib");
+
+describe("absolute", () => {
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+
+  it("should return a positive number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it("should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+});
+
+describe("greet", () => {
+  it("should return a greeting message with name", () => {
+    const result = lib.greet("Ankur");
+    expect(result).toContain("Ankur");
+  });
+});
+
+describe("getCurrencies", () => {
+  it("should return currency list", () => {
+    const result = lib.getCurrencies();
+    expect(result).toEqual(expect.arrayContaining(["USD", "AUD", "EUR"]));
+  });
+});
+
+describe("getProduct", () => {
+  it("should return the product with the given id", () => {
+    const result = lib.getProduct(1);
+    expect(result).toMatchObject({ id: 1, price: 10 });
+  });
+});
