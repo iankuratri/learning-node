50a301da775ef6308c000aa543d51e3bb1e17099
Ankur Atri <ankuratri.in@gmail.com>
2020-07-13 18:20:17 +0530

    jest mock functions added

--- 10-unit-testing/tests/lib.test.js
+++ 10-unit-testing/tests/lib.test.js
@@ -1,4 +1,6 @@
 const lib = require("../lib");
+const db = require("../db");
+const mail = require("../mail");
 
 describe("absolute", () => {
   it("should return a positive number if input is positive", () => {
@@ -52,5 +54,28 @@ describe("registerUser", () => {
     const result = lib.registerUser("ankur");
     expect(result).toMatchObject({ username: "ankur" });
     expect(result.id).toBeGreaterThan(0);
+  });
+});
+
+describe("applyDiscount", () => {
+  it("should apply 10% discount if customer has more than 10 points", () => {
+    db.getCustomerSync = jest.fn().mockReturnValue({ id: 1, points: 20 });
+
+    const order = { customerId: 1, totalPrice: 10 };
+    lib.applyDiscount(order);
+    expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe("notifyCustomer", () => {
+  it("should send email to the customer", () => {
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: "a" });
+    mail.send = jest.fn();
+
+    lib.notifyCustomer({ customerId: 1 });
+
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][0]).toBe("a");
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
